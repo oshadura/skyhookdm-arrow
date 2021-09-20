8b21db816e70afa6f4beeb79c73a7c45177b8d71
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-09-20 15:43:50 -0700

    initial commit

--- cpp/src/skyhook/protocol/skyhook_protocol.h
+++ cpp/src/skyhook/protocol/skyhook_protocol.h
@@ -55,11 +55,11 @@ struct ScanRequest {
 };
 
 /// Utility functions to serialize and deserialize scan requests and result Arrow tables.
-arrow::Status SerializeScanRequest(ScanRequest req, ceph::bufferlist& bl);
-arrow::Status DeserializeScanRequest(ScanRequest& req, ceph::bufferlist bl);
+arrow::Status SerializeScanRequest(ScanRequest req, ceph::bufferlist* bl);
+arrow::Status DeserializeScanRequest(ScanRequest* req, ceph::bufferlist bl);
 arrow::Status SerializeTable(const std::shared_ptr<arrow::Table>& table,
-                             ceph::bufferlist& bl);
-arrow::Status DeserializeTable(arrow::RecordBatchVector& batches, ceph::bufferlist bl,
+                             ceph::bufferlist* bl);
+arrow::Status DeserializeTable(arrow::RecordBatchVector* batches, ceph::bufferlist bl,
                                bool use_threads);
 
 /// Utility function to invoke a RADOS object class function on an RADOS object.
